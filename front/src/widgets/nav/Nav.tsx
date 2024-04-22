8db6435b8c769b04f9f4042724b45017c295f49a
지인성 <crm03008@gmail.com>
2024-04-22 17:31:20 +0900

    feat: 초기 개발 환경 설정, 기본 레이아웃 설정
    - 초기 개발을 위한 환경 설정
    - 기본 레이아웃 세팅 작업

---
+++ front/src/widgets/nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavMenu = () => {
+  return (
+    <div className="w-[20%] max-w-[340px] h-full rounded-r-md border-r-2 border-black shadow-sm">
+      <div className="flex flex-col">
+        <h1 className="text-[2rem] font-bold">대기열 관리</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NavMenu;
