cb652df5ebc48e7fd745db92dc4dc6d6dbf8b2dd
신문영 <ztrl@naver.com>
2024-04-23 20:24:13 +0900

    Add exclusive anonymous filter

--- test/backend/tes24/src/main/java/com/example/tes24/security/filter/ExclusiveFilter.java
+++ test/backend/tes24/src/main/java/com/example/tes24/security/filter/ExclusiveFilter.java
@@ -1,14 +1,14 @@
 package com.example.tes24.security.filter;
 
+import com.example.tes24.properties.AllowedUrlProperties;
+import io.jsonwebtoken.lang.Assert;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.boot.actuate.endpoint.SecurityContext;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedGrantedAuthoritiesUserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,18 +18,32 @@ import java.util.List;
 
 @Component
 public class ExclusiveFilter extends OncePerRequestFilter {
+    private final List<String> anonymous;
+
+    public ExclusiveFilter(AllowedUrlProperties allowedUrlProperties) {
+        Assert.notNull(allowedUrlProperties, "allowedUrlProperties must not be null");
+        Assert.notNull(allowedUrlProperties.getAnonymous(), "anonymous must not be null");
+
+        this.anonymous = allowedUrlProperties.getAnonymous();
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        if (anonymous.contains(requestURI)) {
+            SecurityContextHolder.getContext().setAuthentication(new AnonymousAuthenticationToken(request.getRemoteHost(), new Object(),null ));
+        }
+
         filterChain.doFilter(request, response);
     }
 
     @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    protected boolean shouldNotFilter(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
         if (header == null || header.isBlank()) {
             SecurityContextHolder.getContext().setAuthentication(
                     new AnonymousAuthenticationToken(
-                            "ANONYMOUS", null, List.of(new SimpleGrantedAuthority("ANONYMOUS"))));
+                            "ANONYMOUS", "ANONYMOUS", List.of(new SimpleGrantedAuthority("ANONYMOUS"))));
             return true;
         }
 
