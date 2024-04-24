6970ed20c4cb8d71ebe94475036383fb15c37cc3
신문영 <ztrl@naver.com>
2024-04-24 17:53:19 +0900

    Implement anonymous authentication

--- test/backend/tes24/src/main/java/com/example/tes24/security/filter/AnonymousFilter.java
+++ test/backend/tes24/src/main/java/com/example/tes24/security/filter/AnonymousFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.security.servlet.AntPathRequestMatcherProvider;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,36 +18,32 @@ import java.util.List;
 
 
 @Component
-public class ExclusiveFilter extends OncePerRequestFilter {
+public class AnonymousFilter extends OncePerRequestFilter {
+    private final List<String> allowedUrls;
+    private final List<String> allowedPatterns;
     private final List<String> anonymous;
+    private final AntPathRequestMatcherProvider antPathRequestMatcherProvider = new AntPathRequestMatcherProvider(s -> s);
 
-    public ExclusiveFilter(AllowedUrlProperties allowedUrlProperties) {
+    public AnonymousFilter(AllowedUrlProperties allowedUrlProperties) {
         Assert.notNull(allowedUrlProperties, "allowedUrlProperties must not be null");
         Assert.notNull(allowedUrlProperties.getAnonymous(), "anonymous must not be null");
 
+        this.allowedUrls = allowedUrlProperties.getUrls();
+        this.allowedPatterns = allowedUrlProperties.getPatterns();
         this.anonymous = allowedUrlProperties.getAnonymous();
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
-        if (anonymous.contains(requestURI)) {
-            SecurityContextHolder.getContext().setAuthentication(new AnonymousAuthenticationToken(request.getRemoteHost(), new Object(),null ));
+        if (allowedUrls.contains(requestURI) || anonymous.contains(requestURI) || isMatched(request)) {
+            SecurityContextHolder.getContext().setAuthentication(new AnonymousAuthenticationToken(request.getRemoteHost(), new Object(), List.of(new SimpleGrantedAuthority("ROLE_ANONYMOUS"))));
         }
 
         filterChain.doFilter(request, response);
     }
 
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        String header = request.getHeader("Authorization");
-        if (header == null || header.isBlank()) {
-            SecurityContextHolder.getContext().setAuthentication(
-                    new AnonymousAuthenticationToken(
-                            "ANONYMOUS", "ANONYMOUS", List.of(new SimpleGrantedAuthority("ANONYMOUS"))));
-            return true;
-        }
-
-        return false;
+    private boolean isMatched(HttpServletRequest request) {
+        return allowedPatterns.stream().anyMatch(pattern -> antPathRequestMatcherProvider.getRequestMatcher(pattern).matches(request));
     }
 }
