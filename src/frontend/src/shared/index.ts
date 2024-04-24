081fb49932d55c6d3cd99c9514edd30781be1cd9
DESKTOP-64649GT\Master <crm03008@gmail.com>
2024-04-24 20:35:49 +0900

    delete: 불필요한 파일 삭제
    - 불필요한 파일 삭제

--- src/frontend/src/shared/index.ts
+++ src/frontend/src/shared/index.ts
@@ -3,5 +3,4 @@ export { LinkButton } from "./LinkButton";
 export { cls } from "./cls";
 export { cats, logo } from "./assets";
 export { axiosInstance } from "./axios";
-export { TestComponent } from "./TestComponent";
 export { Loading } from "./Loading";
