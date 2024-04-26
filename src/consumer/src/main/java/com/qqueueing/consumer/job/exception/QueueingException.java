977e0341711b8cc898dd46b6f2c3c29426cb5852
Ehigh <rkd2rb@naver.com>
2024-04-26 13:11:46 +0900

    fix: off batch logic on consumer server

--- src/consumer/src/main/java/com/qqueueing/consumer/job/exception/QueueingException.java
+++ src/consumer/src/main/java/com/qqueueing/consumer/job/exception/QueueingException.java
@@ -1,13 +1,13 @@
-package com.qqueueing.consumer.job.exception;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class QueueingException extends RuntimeException{
-
-    public QueueingException(String msg) {
-        super(msg);
-    }
-}
+//package com.qqueueing.consumer.job.exception;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@NoArgsConstructor
+//public class QueueingException extends RuntimeException{
+//
+//    public QueueingException(String msg) {
+//        super(msg);
+//    }
+//}
