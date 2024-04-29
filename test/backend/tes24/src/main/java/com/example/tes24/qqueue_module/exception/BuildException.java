c206b64696f10efa6595e842cf44cbb507e498f1
신문영 <ztrl@naver.com>
2024-04-29 18:01:36 +0900

    Add BuildException for AbstractQ2AdapterBuilder

---
+++ test/backend/tes24/src/main/java/com/example/tes24/qqueue_module/exception/BuildException.java
@@ -0,0 +1,7 @@
+package com.example.tes24.qqueue_module.exception;
+
+public class BuildException extends RuntimeException{
+    public BuildException(String message) {
+        super(message);
+    }
+}
