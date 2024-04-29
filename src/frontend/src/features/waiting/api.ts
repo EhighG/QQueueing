26405ffbef41dc3f7c859d6b3950fe026f00252f
지인성 <crm03008@gmail.com>
2024-04-29 18:02:24 +0900

    feat: add animation, style improvement, make components
    - add tailwind custom animation
    - style improvement
    - make duplicate element into components

--- src/frontend/src/features/waiting/api.ts
+++ src/frontend/src/features/waiting/api.ts
@@ -1,16 +1,23 @@
+import { WaitingListType } from "@/entities/waitingList/type";
 import { axiosInstance } from "@/shared";
 import { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axiosInstance();
 
-const getWaiting = async () => {
+const getWaitingInfo = async (waitingIdx: number) => {
   return await instance
-    .get("https://jsonplaceholder.typicode.com/todos/1")
+    .post(`/tes24/waiting/${waitingIdx}`, {
+      idVal: waitingIdx,
+    })
     .then(({ data }) => data);
 };
 
-const postWaiting = async (data: Object) => {
-  return await instance.post("").then(({ data }) => data);
+const getWaitingList = async () => {
+  return await instance.get("/queue").then(({ data }) => data);
 };
 
-export { getWaiting, postWaiting };
+const postWaiting = async (data: Omit<WaitingListType, "id">) => {
+  return await instance.post("/queue", data).then(({ data }) => data);
+};
+
+export { getWaitingInfo, getWaitingList, postWaiting };
