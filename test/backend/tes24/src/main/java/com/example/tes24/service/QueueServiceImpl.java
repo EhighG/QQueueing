fc41df2b2ecdf277dc51daee50f3898e80a4b8dc
신문영 <ztrl@naver.com>
2024-04-29 18:02:27 +0900

    Just for test

--- test/backend/tes24/src/main/java/com/example/tes24/service/QueueServiceImpl.java
+++ test/backend/tes24/src/main/java/com/example/tes24/service/QueueServiceImpl.java
@@ -1,10 +1,6 @@
 package com.example.tes24.service;
 
 import com.example.tes24.dto.EnqueueResponse;
-import com.example.tes24.qqueue_module.adapter.Q2Adapter;
-import com.example.tes24.qqueue_module.adapter.Q2AdapterBuilder;
-import com.example.tes24.qqueue_module.dto.Q2ClientRequest;
-import com.example.tes24.qqueue_module.dto.Q2ServerResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +10,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.io.IOException;
 import java.util.concurrent.*;
 
 @Slf4j
@@ -30,38 +25,38 @@ public class QueueServiceImpl implements QueueService {
     @Override
     @Async
     public Future<EnqueueResponse> enqueue() {
-//        RestClient restClient = RestClient.builder()
-//                .requestFactory(new HttpComponentsClientHttpRequestFactory())
-//                .baseUrl("http://" + remoteUrl + ":" + port + "/waiting")
-////                .defaultUriVariables(Map.of("memberId", memberId))
-////                .defaultHeader("Authorization", "Bearer " + token)
-//                .build();
+        RestClient restClient = RestClient.builder()
+                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .baseUrl("http://" + remoteUrl + ":" + port + "/waiting")
+//                .defaultUriVariables(Map.of("memberId", memberId))
+//                .defaultHeader("Authorization", "Bearer " + token)
+                .build();
 
-        Q2Adapter q2Adapter =
-                Q2AdapterBuilder.builder()
-                        .url("http://" + remoteUrl + ":" + port + "/waiting")
-                        .method("POST")
-                        .build();
+        return CompletableFuture.completedFuture(restClient.post().retrieve().body(EnqueueResponse.class));
 
-        Q2ClientRequest q2ClientRequest = new Q2ClientRequest();
-//        q2ClientRequest.setClientId();
-//        q2ClientRequest.setClientKey();
-//        q2ClientRequest.setUserId();
-//        q2ClientRequest.setUserKey();
-
-        try {
-            Q2ServerResponse q2ServerResponse = q2Adapter.enqueue(q2ClientRequest);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-        return null;
+//        Q2Adapter q2Adapter =
+//                Q2AdapterBuilder.builder()
+//                        .url("http://" + remoteUrl + ":" + port + "/tes24")
+//                        .method("POST")
+//                        .build();
+//
+//        Q2ClientRequest q2ClientRequest = new Q2ClientRequest();
+//        q2ClientRequest.setClientId("1");
+//        q2ClientRequest.setClientKey("2");
+//        q2ClientRequest.setUserId("3");
+//        q2ClientRequest.setUserKey("4");
+//j
+//        try {
+//            Q2ServerResponse q2ServerResponse = q2Adapter.enqueue(q2ClientRequest);
+//            log.info(String.valueOf(q2ServerResponse));
+//        } catch (IOException e) {
+//            log.error(e.getMessage());
+//        }
 
 //        try {
 //            Thread.sleep(1000);
 //        } catch (InterruptedException e) {}
 //        return CompletableFuture.completedFuture(new EnqueueResponse(0L, "id"));
-
-//        return CompletableFuture.completedFuture(restClient.post().retrieve().body(EnqueueResponse.class));
     }
 
     @Override
