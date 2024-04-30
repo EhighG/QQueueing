f0c2de50a4488cb78c155f99d1368b7d4ca9bdce
SYH <syhuni0314@gmail.com>
2024-04-30 11:16:12 +0900

    feat: 등록 과정 통합

--- src/main/src/main/java/com/qqueueing/main/registration/model/Registration.java
+++ src/main/src/main/java/com/qqueueing/main/registration/model/Registration.java
@@ -3,6 +3,7 @@ package com.qqueueing.main.registration.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,12 +16,20 @@ public class Registration {
 
     @Id
     private String id; // 자동 생성되는 식별자
+    private String topicName;
+    private String targetUrl;
 
-    private String registrationUrl; // 등록 URL
     private Integer maxCapacity; // 최대 수용 인원
     private Integer processingPerMinute; // 1분당 처리 인원
     private String serviceName; // 서비스명
     private String queueImageUrl; // 대기열 이미지
+    @Setter
     private Boolean isActive; // 활성화 여부
+
+    public Registration(String topicName, String targetUrl, boolean isActive) {
+        this.topicName = topicName;
+        this.targetUrl = targetUrl;
+        this.isActive = isActive;
+    }
 
 }
--- src/main/src/main/java/com/qqueueing/main/registration/service/RegistrationService.java
+++ src/main/src/main/java/com/qqueueing/main/registration/service/RegistrationService.java
@@ -2,6 +2,7 @@ package com.qqueueing.main.registration.service;
 
 import com.qqueueing.main.registration.model.Registration;
 import com.qqueueing.main.registration.repository.RegistrationRepository;
+import com.qqueueing.main.waiting.service.WaitingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,17 @@ public class RegistrationService {
 
 //    @Autowired
     private RegistrationRepository registrationRepository;
+    private final WaitingService waitingService;
 
-    public RegistrationService(RegistrationRepository registrationRepository) {
+    public RegistrationService(RegistrationRepository registrationRepository, WaitingService waitingService) {
         this.registrationRepository = registrationRepository;
+        this.waitingService = waitingService;
     }
 
     public Registration createRegistration(Registration registration) {
-        return registrationRepository.save(registration);
+
+        registrationRepository.save(registration);
+        waitingService.activate(registration.getTopicName());
     }
 
     public List<Registration> getAllRegistrations() {
