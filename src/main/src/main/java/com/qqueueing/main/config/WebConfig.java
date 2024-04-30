581540626142662a667026bbf009d376bbcd8327
Ehigh <rkd2rb@naver.com>
2024-04-30 15:33:00 +0900

    feat: add CORs setting

--- src/main/src/main/java/com/qqueueing/main/config/WebConfig.java
+++ src/main/src/main/java/com/qqueueing/main/config/WebConfig.java
@@ -4,11 +4,24 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//                .allowedOrigins("https://k10a401.p.ssafy.io",
+//                        "http://localhost:3000", "http://localhost:4173")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(false);
+    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
