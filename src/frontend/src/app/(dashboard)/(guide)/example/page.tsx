6ba6c0d13c60c3674a7ebc852d335219ebc8543d
지인성 <crm03008@gmail.com>
2024-05-03 17:46:23 +0900

    refactor: separate guide page into component
    - separate guide page into component

--- src/frontend/src/app/(dashboard)/(guide)/example/page.tsx
+++ src/frontend/src/app/(dashboard)/(guide)/example/page.tsx
@@ -1,7 +1,14 @@
+import { ExamplePage } from "@/pages-flat";
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "Example",
+  description: "큐잉 Example 페이지.",
+};
+
 const Page = () => {
-  return <div>Example Page</div>;
+  return <ExamplePage />;
 };
 
 export default Page;
---
+++ src/frontend/src/pages-flat/DocsPage.tsx
@@ -0,0 +1,12 @@
+import { SectionTitle } from "@/shared";
+import React from "react";
+
+const DocsPage = () => {
+  return (
+    <div className="flex flex-col flex-1 gap-2 bg-white rounded-md border">
+      <SectionTitle title="Docs" />
+    </div>
+  );
+};
+
+export default DocsPage;
