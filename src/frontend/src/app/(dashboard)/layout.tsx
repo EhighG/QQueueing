4f564bbc7bdc255ee9d3bb4a486af35c0256416b
지인성 <crm03008@gmail.com>
2024-05-03 10:34:15 +0900

    rename: moving folder for independent management of queue pages
    - moving folder for independent management of queue pages

---
+++ src/frontend/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import { Header, NavMenu } from "@/widgets";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      <main>
+        <NavMenu />
+        <div className="flex flex-col flex-1">
+          <div className="flex flex-col flex-1 max-2xl:m-5 m-10 gap-10">
+            {children}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
