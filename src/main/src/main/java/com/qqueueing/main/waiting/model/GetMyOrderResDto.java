a6d2f37ec6798e4c8ff2d197d8412d08430ad35c
Ehigh <rkd2rb@naver.com>
2024-05-06 20:56:28 +0900

    refactor: divide forwarding logic from enter queue, getMyOrder

--- src/main/src/main/java/com/qqueueing/main/waiting/model/GetMyOrderResDto.java
+++ src/main/src/main/java/com/qqueueing/main/waiting/model/GetMyOrderResDto.java
@@ -8,10 +8,18 @@ import lombok.Setter;
 public class GetMyOrderResDto {
     private Long myOrder;
     private Integer totalQueueSize;
+    @Setter
+    private String tempToken;
 
     // on waiting
     public GetMyOrderResDto(Long myOrder, Integer totalQueueSize) {
         this.myOrder = myOrder;
         this.totalQueueSize = totalQueueSize;
     }
+//
+//    public GetMyOrderResDto(Long myOrder, Integer totalQueueSize, String tempToken) {
+//        this.myOrder = myOrder;
+//        this.totalQueueSize = totalQueueSize;
+//        this.tempToken = tempToken;
+//    }
 }
