fcf3bb1f80ddd0fb1b8f5432ab111046d8cb578b
Zerotay <donggun1000@naver.com>
2024-05-07 05:58:00 +0000

    feat: add deletion script

--- src/pipes/conf.sh
+++ src/pipes/conf.sh
@@ -1,58 +1,73 @@
 #!/bin/bash
-echo "set nginx!!"
-#0 set variable
-CONTAINER_NAME="test-nginx"
-NGINX_PATH="/etc/nginx"
-URL_PATH=$1
-COMPLETE_FILE="/complete.conf"
-
-# this is only for test. must not forget
-echo "initialize nginx for test"
-GIT_ROOT=$(git rev-parse --show-toplevel)
-$GIT_ROOT/test/test.sh nginx -d
-docker network connect qqueueing_qqueueing-network $CONTAINER_NAME
-
-
-if [[ -z $1 ]]; then
-	echo "no input"
+if [[ -z $2 ]];then
+	echo "needed exact 2 arguements"
 	exit 1
 fi
-
-
 #0 set variable
 CONTAINER_NAME="test-nginx"
 NGINX_PATH="/etc/nginx"
-URL_PATH=$1
+URL_PATH=$2
 COMPLETE_FILE="/complete.conf"
 
-# this is for test
-sudo rm -rf $NGINX_PATH
 
-#1 copy nginx files from container
-# todo: if user have nginx in host, need to handle that
-if test -d $NGINX_PATH
-then echo "path already exists"
-	exit 0
-fi
-sudo docker cp $CONTAINER_NAME:/etc/nginx $NGINX_PATH
-sudo touch $NGINX_PATH$COMPLETE_FILE
-#ls -al $NGINX_PATH
+case $1 in
+	register)
+		echo "set nginx!!"
 
-# if in first setting, nginx container must include our network
+		# this is only for test. must not forget
+		echo "initialize nginx for test"
+		GIT_ROOT=$(git rev-parse --show-toplevel)
+		$GIT_ROOT/test/test.sh nginx -d
+		docker network connect qqueueing_qqueueing-network $CONTAINER_NAME
+		# this is for test
+		sudo rm -rf $NGINX_PATH
 
-#2 execute python script
-sudo python3 nginx.py $URL_PATH
-sudo chmod 664 $NGINX_PATH$COMPLETE_FILE
-sudo cat $NGINX_PATH$COMPLETE_FILE | grep $URL_PATH
 
-#3 copy completed file to contianer
-sudo docker exec $CONTAINER_NAME mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.save
-sudo docker cp $NGINX_PATH$COMPLETE_FILE $CONTAINER_NAME:/etc/nginx/nginx.conf
 
-sudo rm -rf $NGINX_PATH
+		#1 copy nginx files from container
+		# todo: if user have nginx in host, need to handle that
+		if test -d $NGINX_PATH
+		then echo "path already exists"
+			exit 0
+		fi
+		sudo docker cp $CONTAINER_NAME:/etc/nginx $NGINX_PATH
+		sudo touch $NGINX_PATH$COMPLETE_FILE
 
-sudo docker exec $CONTAINER_NAME nginx -t 
-#exit 1
+		# if in first setting, nginx container must include our network
 
-#4 restart nginx
-sudo docker exec $CONTAINER_NAME nginx -s reload
+		#2 execute python script
+		sudo python3 nginx.py $URL_PATH
+		sudo chmod 664 $NGINX_PATH$COMPLETE_FILE
+		sudo cat $NGINX_PATH$COMPLETE_FILE | grep $URL_PATH
+
+		#3 copy completed file to contianer 
+		sudo docker exec $CONTAINER_NAME mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.save
+		sudo docker cp $NGINX_PATH$COMPLETE_FILE $CONTAINER_NAME:/etc/nginx/nginx.conf
+
+		sudo rm -rf $NGINX_PATH
+
+		sudo docker exec $CONTAINER_NAME nginx -t 
+		#exit 1
+
+		#4 restart nginx
+		sudo docker exec $CONTAINER_NAME nginx -s reload
+		exit 0
+		;;
+	delete)
+		echo "delete"
+		sudo docker exec $CONTAINER_NAME rm /etc/nginx/nginx.conf
+		sudo docker exec $CONTAINER_NAME mv /etc/nginx/nginx.conf.save /etc/nginx/nginx.conf
+		sudo docker exec $CONTAINER_NAME nginx -s reload
+		;;
+	modify)
+		echo "modify"
+		;;
+	*)
+		echo "Not valid arguement"
+		exit 1
+		;;
+esac
+
+
+
+
