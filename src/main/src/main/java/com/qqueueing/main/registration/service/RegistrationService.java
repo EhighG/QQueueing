2e1e9458fe303f5366c2384324c8d2b5633c666b
SYH <syhuni0314@gmail.com>
2024-05-07 10:04:41 +0900

    feat: 등록 URL 상세조회 api 구현

--- src/main/src/main/java/com/qqueueing/main/registration/service/RegistrationService.java
+++ src/main/src/main/java/com/qqueueing/main/registration/service/RegistrationService.java
@@ -3,6 +3,7 @@ package com.qqueueing.main.registration.service;
 import com.qqueueing.main.registration.model.Registration;
 import com.qqueueing.main.registration.repository.RegistrationRepository;
 import com.qqueueing.main.waiting.service.WaitingService;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,4 +25,23 @@ public class RegistrationService {
     public List<Registration> getAllRegistrations() {
         return registrationRepository.findAll();
     }
+
+    public Registration getRegistrationById(String id) throws ChangeSetPersister.NotFoundException {
+        return registrationRepository.findById(id)
+                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+    }
+
+//    public Registration updateRegistrationById(String id, RegistrationUpdateRequest request) throws ChangeSetPersister.NotFoundException {
+//        Registration registration = registrationRepository.findById(id)
+//                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+//        registration.update(request.getName(), request.getEmail(), request.getPhoneNumber());
+//        return registrationRepository.save(registration);
+//    }
+//    public Registration createRegistration(Registration registration) {
+//        return registrationRepository.save(registration);
+//    }
+//    public Registration createRegistration(Registration registration) {
+//        return registrationRepository.save(registration);
+//    }
+
 }
