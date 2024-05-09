66aab72adfbaa568c16a46b8dfb0a6113b9c4d57
지인성 <crm03008@gmail.com>
2024-05-09 15:23:33 +0900

    feat: redirect another page for each component via index
    - redirect another page for each component via index

--- test/frontend/src/pages-flat/MainTestPage.tsx
+++ test/frontend/src/pages-flat/MainTestPage.tsx
@@ -5,14 +5,9 @@ const MainTest = () => {
   return (
     <div className="flex flex-col w-full h-full items-center justify-center p-5">
       <div className="grid grid-cols-4 grid-rows-2 gap-5">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {Array.from({ length: 10 }, (_, index) => (
+          <Card key={index + 1} productId={index + 1} />
+        ))}
       </div>
     </div>
   );
