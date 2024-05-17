328f044cadee7e24a2bad7d5c00e48c6ce4ab3c3
신문영 <ztrl@naver.com>
2024-05-17 10:39:48 +0900

    feat: Add RequestException

---
+++ test/backend/tes24/src/main/java/com/example/tes24/qqueueing/exception/RequestException.java
@@ -0,0 +1,7 @@
+package com.example.tes24.qqueueing.exception;
+
+public class RequestException extends RuntimeException {
+    public RequestException(Throwable cause) {
+        super(cause);
+    }
+}
