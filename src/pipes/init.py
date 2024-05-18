2296bc39b9596a11c5fc563a8d516156c0bda1f8
Zerotay <donggun1000@naver.com>
2024-05-18 11:03:44 +0000

    feat: unset browser caching

--- src/pipes/init.py
+++ src/pipes/init.py
@@ -63,6 +63,14 @@ def main():
                 Statement('proxy_pass http://qqueueing-main:8081$request_uri;')
             ])
         )
+        server.push(
+                ind=top,
+                val=Statement('add_header Cache-Control "max-age=0, no-cache, no-store, must-revalidate";')
+        )
+        server.push(
+                ind=top,
+                val=Statement('add_header Pragma "no-cache";')
+        )
 
 
     real_location = None
